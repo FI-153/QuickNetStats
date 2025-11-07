cbb99840b594d176b9a268c3ac02eea0eb256fe4
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-11-07 22:09:32 +0100

    Initial Commit

---
+++ QuickNetStats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickNetStats
+//
+//  Created by Federico Imberti on 2025-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickNetStats/QuickNetStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNetStatsApp.swift
+//  QuickNetStats
+//
+//  Created by Federico Imberti on 2025-11-07.
+//
+
+import SwiftUI
+
+@main
+struct QuickNetStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
