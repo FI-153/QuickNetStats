c8aa329082991cfa685f495520d7b7a76ac5ba19
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-12-14 10:00:17 +0100

    Useless space

--- QuickNetStats/Models/NetworkStats.swift
+++ QuickNetStats/Models/NetworkStats.swift
@@ -62,7 +62,7 @@ struct NetworkStats {
         if #available(macOS 26, *) {
             if let linkQuality = linkQuality {
                 if linkQuality != .unknown {
-                    quality = "\(linkQuality.rawValue.capitalized) "
+                    quality = "\(linkQuality.rawValue.capitalized)"
                 }
             }
         }
