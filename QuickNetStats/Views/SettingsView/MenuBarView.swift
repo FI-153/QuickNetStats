f707d796636ddb751a29cb0727c1afe09ffc43a8
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-12-15 17:24:01 +0100

    Disabled menu bar toggles when the summary is hidden

--- QuickNetStats/Views/SettingsView/MenuBarView.swift
+++ QuickNetStats/Views/SettingsView/MenuBarView.swift
@@ -15,8 +15,16 @@ struct MenuBarView: View {
     var body: some View {
         Form {
             Section {
-                ToggleView(title: "Show Summary", variable: settings.$showSummaryInMenu, description: "Display a summary of the connection or just an icon")
-                ToggleView(title: "Show Network quality", variable: settings.$showQualityInMenu, description: "Display a description of the network quality or not")
+                ToggleView(title: "Show Summary",
+                           variable: settings.$showSummaryInMenu,
+                           description: "Display a summary of the connection or just an icon"
+                )
+                
+                ToggleView(title: "Show Network quality",
+                           variable: settings.$showQualityInMenu,
+                           description: "Display a description of the network quality or not",
+                           isDisabled: !settings.showSummaryInMenu
+                )
             } header: {
                 Text("Menu Bar Style")
             }
